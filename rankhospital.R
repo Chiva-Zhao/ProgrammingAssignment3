fe3f56397572a90e4dbe3cb338919f0c303d65b0
zhenhua <zhaozhenhua88@gmail.com>
2014-08-01 14:38:14 +0800

    Adding randhospital.R
    Adding randhospital.R

--- rankhospital.R
+++ rankhospital.R
@@ -1,14 +1,15 @@
-best <- function(state, outcome) {
+rankhospital <- function(state, outcome, num = "best") {
     ## Read outcome data
     ## Check that state and outcome are valid
-    ## Return hospital name in that state with lowest 30-day death
-    ## rate
-    data <- read.csv("outcome-of-care-measures.csv", colClasses = "character",na.strings="Not Available")
-    stateData <- unique(data[,7])
-    if(!state %in% stateData)
+    ## Return hospital name in that state with the given rank
+    ## 30-day death rate
+    file <- "outcome-of-care-measures.csv"
+    hospitalName <- 'Hospital.Name'
+    data <- read.csv(file,colClasses = "character")
+    states <- unique(data[,7])
+    if(!state %in% states)
         stop("invalid state")
     validOutcome <- c('heart attack','heart failure','pneumonia')
-    
     if(!outcome %in% validOutcome)
         stop("invalid outcome")
     colname <- ''
@@ -21,7 +22,18 @@ best <- function(state, outcome) {
     data[,colname]<-as.numeric(data[,colname])
     data1<-subset(data,data[,'State'] == state)
     data2<-subset(data1,!is.na(data1[colname]))
-    minValue = min(data2[,colname],na.rm=T)
-    result1 = subset(data2,data2[,colname] == minValue)
-    sort(result1$Hospital.Name)[1]
-} 
+    data3<- data2[order(data2[,colname],data2[,hospitalName]),]
+    if(is.character(num)){
+        if(num == 'best'){
+            data3[1,hospitalName]
+        }else if(num == 'worst'){
+            data3[nrow(data3),hospitalName]
+        }
+    }else if(is.numeric(num)){
+        data3[num,hospitalName]
+    }else
+        NA 
+        
+
+    
+}
